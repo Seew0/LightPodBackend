cb4300cc5f0650d4eed01bc8b81e5329b76ee03b
seew0 <miglanidevansh83@gmail.com>
2024-11-12 13:23:23 +0530

    connect

--- routes/index.ts
+++ routes/index.ts
@@ -1,13 +1,18 @@
 import express from 'express';
 import userRoutes from './user';
 import serviceRoutes from "./services";
+import logsRoutes from "./logs";
+import apiRoutes from "./api"; 
 
 
 
 const app = express.Router();
 
 app.use("/user", userRoutes);
+
 app.use("/services", serviceRoutes);
+app.use("/logs",logsRoutes);
+app.use("/v1",apiRoutes);
 
 
 
